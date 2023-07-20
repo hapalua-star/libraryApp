11e81a11f9ebc56f9b91f0fd6ccd4bc4c003cff7
hapalua-star <69691701+hapalua-star@users.noreply.github.com>
2023-07-20 11:28:28 -0400

    add liba\

---
+++ javascript.js
@@ -0,0 +1,37 @@
+
+let myLibrary=[];
+
+
+
+
+function Book(title, author,pages,read) {                //the constructor function
+    this.title = title;
+    this.author = author;
+    this.pages=pages;
+    this.read=read;
+    this.information=function(){
+        return((this.title) +" by "+this.author + "it has" + this.pages + "it was" + this.read);
+    };
+
+};
+
+
+
+
+function addBookToLibrary() {
+    const title1=prompt("what is the title of the book");
+    const author1=prompt("what is author of book");
+    const pages1=prompt("how many pages is it");
+    const read1=prompt("was it read");
+   
+    const Booked=new Book(title1,author1,pages1,read1);
+    myLibrary.push(Booked);
+ };
+
+addBookToLibrary();
+console.log(myLibrary[0].information());
+
+
+  
+  
+  
